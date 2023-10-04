c179b06f809366a4f7a7ef82c04cc23c9ae704b5
sarvarpython <146908669+sarvarpython@users.noreply.github.com>
2023-10-04 10:31:42 +0500

    lug'atlar bilan ishlash

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+'''  DICTIONARY - Lug'atlar bilan ishlash '''
+
+
+
+# car_0 = {'model':'jetaur','rang':'qora'}
+# print(car_0['model'])
+# print(car_0['rang'])
+
+# ''' run qilganimizda JETAUR QORA degan so'z chiqadi '''
+
+# talaba = {'ism-sharif':'sarvar nematov','t_yil':2004,'yoshi':19}
+# print(f" TALABA {talaba['ism-sharif'].title()},{talaba['t_yil']} da tug'ilgan \
+#   u hozir {talaba['yoshi']}")
+#   #LUG'ATGA ELEMENT QUSHISH
+# talaba['kurs'] = 3
+# talaba['ovqati'] = 'shurva'
+# talaba2 = talaba.get('t_yil')
+# print(talaba2)
+
