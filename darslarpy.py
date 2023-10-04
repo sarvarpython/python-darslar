b3b0328b77e983d66ee519a8f095cdf4c1998a83
sarvarpython <146908669+sarvarpython@users.noreply.github.com>
2023-10-04 09:27:40 +0500

    animatsiya kodi pythonda

---
+++ darslarpy.py
@@ -0,0 +1,11 @@
+
+# import turtle
+# animation = turtle.Turtle()
+# animation.speed(40)
+# animation.hideturtle()
+# animation.getscreen().bgcolor("black")
+# animation.color("aqua")
+
+# for i in range(17):
+#     animation.circle(i)
+#     animation._rotate(5)
